055c96f8cb3bcff0fdc4359a0d3f744cf710e423
Samir Triande <habib.triande@gmail.com>
2020-04-27 01:30:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,15 +7,15 @@ arr = [1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
-arr << "5"
+  arr << "5"
 end
 
 def add_element_to_start_of_array(array, element)
-
+  arr.unshift("0")
 end
 
 def remove_element_from_end_of_array(array)
-
+  arr.pop
 end
 
 def remove_element_from_start_of_array(array)
